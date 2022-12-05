467853acd02755b846014ffaa6f19beb045a19d8
KrisKotowski <kkotowski777@gmail.com>
2022-12-05 22:41:53 +0100

    Update sql_changelog.sql

--- sql_changelog.sql
+++ sql_changelog.sql
@@ -2,4 +2,6 @@ alter table broker add api_key character varying(255);
 
 insert into broker (broker_id, descr, webpage, api_key) values (3,'TraderMade', 'https://tradermade.com/','DlZmo2SbGyCjzcXyaaPw');
 
+insert into broker (broker_id, descr, webpage, api_key) values (4,'TradingEconomics', 'https://tradingeconomics.com/','');
+
 commit;
