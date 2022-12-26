c284087ab0ce0ffd04d83e5c21530560995e41b3
KrisKotowski <kkotowski777@gmail.com>
2022-12-26 15:52:08 +0100

    Update sql_changelog.sql

--- sql_changelog.sql
+++ sql_changelog.sql
@@ -1,2 +1,6 @@
 insert into broker (broker_id, descr, webpage, status, api_key, class_name) values (9,'Wise', 'https://wise.com/','1','','ScrapeWise');
 
+drop table rate cascade;
+alter table hist drop rate_id cascade;
+alter table hist RENAME COLUMN buy_rate TO buy_qty; 
+alter table hist RENAME COLUMN sell_rate TO sell_qty; 
